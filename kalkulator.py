c3c30db7cdd8f0d5add18abb9fecd8a2063c24aa
jeczenmaciej <167018401+jeczenmaciej@users.noreply.github.com>
2024-04-22 22:32:08 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,11 +1,9 @@
 import logging
 
 def main():
-    # Konfiguracja loggera
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    # Wybór działania
     print("Podaj działanie, posługując się odpowiednią liczbą:")
     print("1. Dodawanie")
     print("2. Odejmowanie")
@@ -13,11 +11,9 @@ def main():
     print("4. Dzielenie")
     operation = int(input("Wybierz numer działania: "))
 
-    # Pobieranie dwóch liczb
     num1 = float(input("Podaj pierwszą liczbę: "))
     num2 = float(input("Podaj drugą liczbę: "))
 
-    # Wykonanie obliczenia
     if operation == 1:
         result = num1 + num2
         operation_name = "Dodawanie"
@@ -35,10 +31,7 @@ def main():
             logger.error("Nie można dzielić przez zero!")
             return
 
-    # Logowanie informacji o działaniu
     logger.info(f"{operation_name}: {num1} i {num2}")
-
-    # Wyświetlenie wyniku
     print(f"Wynik to {result:.2f}")
 
 if __name__ == "__main__":
